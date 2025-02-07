d04124e208f567c3a91ac1505b5b88648cf51c4e
Jose Garcia <josegarcia19809@gmail.com>
2025-02-07 07:11:30 -0600

    Programación Orientada a Aspectos (AOP): Ordenando los Aspectos con @Order

---
+++ src/main/java/com/example/spring_boot_aop/aop/GreetingFooAspect.java
@@ -0,0 +1,33 @@
+package com.example.spring_boot_aop.aop;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Order(1)
+@Component
+@Aspect
+public class GreetingFooAspect {
+    private Logger logger = LoggerFactory.getLogger(GreetingFooAspect.class);
+
+    @Before("execution(* com.example.spring_boot_aop.services.GreetingService.*(..))")
+    public void loggerBefore(JoinPoint joinPoint) {
+        String methodName = joinPoint.getSignature().getName();
+        String args = Arrays.toString(joinPoint.getArgs());
+        logger.info("Antes Foo: " + methodName + " invocado con los argumentos: " + args);
+    }
+
+    @After("execution(* com.example.spring_boot_aop.services.GreetingService.*(..))")
+    public void loggerAfter(JoinPoint joinPoint) {
+        String methodName = joinPoint.getSignature().getName();
+        String args = Arrays.toString(joinPoint.getArgs());
+        logger.info("Después Foo: " + methodName + " con los argumentos: " + args);
+    }
+}
